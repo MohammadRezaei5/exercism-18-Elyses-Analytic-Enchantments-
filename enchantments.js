6aad7a8b5777cbc076917ae4352ab93e942b1c05
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-07 08:33:53 +0330

    Function: doesStackIncludeCard

--- enchantments.js
+++ enchantments.js
@@ -7,3 +7,13 @@ function getCardPosition(stack, card) {
 }
 
 // console.log(getCardPosition([9, 7, 3, 2], card));
+
+///---///
+
+// const card = 3;
+
+function doesStackIncludeCard(stack, card) {
+  return stack.includes(card);
+}
+
+// console.log(doesStackIncludeCard([2, 3, 4, 5], card));
